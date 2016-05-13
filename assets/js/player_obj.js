84856fb61329f2d39ad88fe7e03cb0f5482ab2f1
Kevin <kevinschuurmans@msn.com>
2016-05-13 19:37:53 +0200

    added player shape
    be it x or o

--- assets/js/player_obj.js
+++ assets/js/player_obj.js
@@ -1,4 +1,20 @@
-function Player()
+const SHAPE_IMAGES =
 {
+  'x': 'assets/img/cross.png',
+  'o': 'assets/img/circle.png',
+}
 
+function Player(name)
+{
+  this.name = name;
+}
+
+Player.prototype.getShape = function()
+{
+  return this.shape;
+}
+
+Player.prototype.getShapeImage = function()
+{
+  return SHAPE_IMAGES[ this.shape ];
 }
--- assets/js/square_obj.js
+++ assets/js/square_obj.js
@@ -1,24 +1,18 @@
-var symbols =
-{
-  'x': 'assets/img/cross.png',
-  'o': 'assets/img/circle.png',
-}
-
 function Square()
 {
   this.x = x;
   this.y = y;
-  this.symbol = '';
+  this.player = null;
 }
 
-Square.prototype.place = function(symbol)
+Square.prototype.place = function( player )
 {
-  this.symbol = symbol;
-  this.html.attr('src', symbols[symbol]);
+  this.player = player;
+  this.html.attr( 'src', player.getShapeImage() );
 }
 
 Square.prototype.clear = function()
 {
-  this.symbol = null;
-  //this.html.attr('src', null);
+  this.player = null;
+  this.html.attr( 'src', null );
 }
